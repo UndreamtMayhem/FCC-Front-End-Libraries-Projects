57efe870386e30bfedf7b3264281eefe16098385
Daniel UndreamtMayhem Marcial Johnson <danny_boyx@hotmail.co.uk>
2019-02-25 15:08:55 +0000

    basic layout structures

--- 3. Build a Drum Machine/drum-machine/src/App.js
+++ 3. Build a Drum Machine/drum-machine/src/App.js
@@ -1,18 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+const soundBank = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  }, {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  }, {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  }, {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  }, {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  }, {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  }, {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  }, {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  }, {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+class Display extends React.Component {
+  render() {
+    return <header id="display" className="App-header">
+      <p>Display from component</p>
+      <p>To be implemented</p>
+    </header>
+
+  }
+}
+
+class PadBank extends React.Component {
+  render() {
+    return <div className="padBank">Hello, {this.props.name}
+    <p>TODO</p>
+    </div >;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div id="drum-machine" className="App">
-        <p>Drum machine</p>
-        <header id="display" className="App-header">
-         <p>Display</p>
-        </header>
-        <div className="padBank">
-        <p>padBank</p>
-        </div>
+       
+        <Display/>
+        <PadBank name="Sara"/>
       </div>
     );
   }
